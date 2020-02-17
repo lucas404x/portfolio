93e93fdec46d95229fb9cbc0af3d5cf250a48264
lucas404x <joselucas0303@gmail.com>
2020-02-17 13:58:21 -0300

    contador de palavras adicionado

--- scripts/contato.js
+++ scripts/contato.js
@@ -1,14 +1,18 @@
-
-let text = document.getElementById("msg");
-
 function alterarAlinhamento() {
-    text.setAttribute("placeholder", "");
-    text.style.textAlign = "justify";
+  text.setAttribute("placeholder", "");
+  text.style.textAlign = "justify";
 }
 
 function voltarOriginal() {
-  if(text.value.length == 0){
+  if (text.value.length == 0){
     text.setAttribute("placeholder", "Digite sua mensagem");
     text.style.textAlign = "center";
-  }
+    }
 }
+
+function limite() {
+  document.getElementById("contador").innerHTML = `${text.value.length}/100`;
+}
+
+
+let text = document.getElementById("msg");
