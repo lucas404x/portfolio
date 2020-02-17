f3ae26885c9cd05807e47686da3c52c13e54afa7
lucas404x <joselucas0303@gmail.com>
2020-02-17 14:24:08 -0300

    bugs corrigidos

--- scripts/contato.js
+++ scripts/contato.js
@@ -7,11 +7,19 @@ function voltarOriginal() {
   if (text.value.length == 0){
     text.setAttribute("placeholder", "Digite sua mensagem");
     text.style.textAlign = "center";
+    document.getElementById("contador").innerHTML = '0/100';
     }
+
 }
 
 function limite() {
-  document.getElementById("contador").innerHTML = `${text.value.length}/100`;
+  document.getElementById("contador").innerHTML = `${text.value.length + 1}/100`;
+}
+
+function backsp_limite() {
+  if (event.key == "Backspace") {
+    document.getElementById("contador").innerHTML = text.value.length > 0 ? `${text.value.length - 1}/100` : "0/100";
+  }
 }
 
 
